fc44142a86192699fb6610a10b21ea086b40f2e1
tianxun <zhoutianxun@gmail.com>
2022-04-28 22:14:29 +0800

    fix bug in main

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
     bar = tqdm(dataset, total=len(dataset))
     for i, data in enumerate(bar):
         x, y = data
-        success, perturbation, nfev, attack_image = op_attack_fast.attack(x.clone().unsqueeze(0), y.view(1), pixels=args.pixels, maxiter=75, mask=None)
+        success, perturbation, nfev, attack_image = op_attack_fast.attack(x.clone().unsqueeze(0), y.view(1), pixels=args.pixels, maxiter=75, mask=mask)
         correct += (1-success)
         total += 1
         nfevs += nfev
